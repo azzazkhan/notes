1b8e7e66197537c449541de221d4e6cf9cfaf466
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2024-03-03 18:23:48 +0500

    Updated shell setup script

--- wsl/08-setup-shell-helpers.sh
+++ wsl/08-setup-shell-helpers.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "\n\n"  >> ~/.zshrc \
-    && echo "export PATH=\"\$PATH:\$(yarn global bin):\$(composer global config bin-dir --absolute --quiet)\"\n" >> ~/.zshrc \
-    && echo "alias artisan=\"php artisan\"\n" \
-    && echo "alias mfs=\"php artisan migrate:fresh --seed\"\n" \
-    && echo "alias artc=\"php artisan optimize:clear && php artisan clear-compiled\""
+
+echo "\n\n" >> ~/.zshrc
+cat << EOF >> ~/.zshrc
+export PATH="\$PATH:$(yarn global bin):\$(composer global config bin-dir --absolute --quiet)"
+
+alias artisan="php artisan"
+alias mfs="php artisan migrate:fresh --seed"
+alias artc="php artisan optimize:clear && php artisan clear-compiled"
+alias dc="docker compose"
+alias sail="./sail"
+alias install-sail="curl -s \"https://raw.githubusercontent.com/azzazkhan/devcontainer-laravel/master/install\" | bash"
+EOF
