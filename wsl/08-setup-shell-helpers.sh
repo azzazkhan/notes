392f99f4e68ea505dd81110b8765e990c8bc4ec9
Azzaz <25636920+azzazkhan@users.noreply.github.com>
2024-06-19 20:41:32 +0500

    fix: Fixed missing command escape in shell helper script

--- wsl/08-setup-shell-helpers.sh
+++ wsl/08-setup-shell-helpers.sh
@@ -3,12 +3,9 @@
 
 echo "\n\n" >> ~/.zshrc
 cat << EOF >> ~/.zshrc
-export PATH="\$PATH:$(yarn global bin):\$(composer global config bin-dir --absolute --quiet)"
+export PATH="\$PATH:\$(yarn global bin):\$(composer global config bin-dir --absolute --quiet)"
 
 alias artisan="php artisan"
-alias mfs="php artisan migrate:fresh --seed"
-alias artc="php artisan optimize:clear && php artisan clear-compiled"
-alias dc="docker compose"
 alias sail="./sail"
 alias install-sail="curl -s \"https://raw.githubusercontent.com/azzazkhan/devcontainer-laravel/master/install\" | bash"
 EOF
